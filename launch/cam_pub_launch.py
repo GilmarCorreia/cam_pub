3150ab5b1149f0081c99e2fefb79c40fb33ee42b
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2026-06-14 19:21:09 -0300

    fix: changing to flake

--- launch/cam_pub_launch.py
+++ launch/cam_pub_launch.py
@@ -8,46 +8,49 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     default_param_file = PathJoinSubstitution(
-        [get_package_share_directory("cam_pub"), "config", "webcam.yaml"]
+        [get_package_share_directory('cam_pub'), 'config', 'webcam.yaml']
     )
 
-    namespace = LaunchConfiguration("namespace", default="")
-    threaded = LaunchConfiguration("threaded", default="false")
-    param_file_path = LaunchConfiguration("param_file_path", default=default_param_file)
+    namespace = LaunchConfiguration('namespace', default='')
+    threaded = LaunchConfiguration('threaded', default='false')
+    param_file_path = LaunchConfiguration(
+        'param_file_path',
+        default=default_param_file
+    )
 
     return LaunchDescription(
         [
             DeclareLaunchArgument(
-                "namespace",
-                default_value="",
-                description="ROS namespace for the cam_pub node",
+                'namespace',
+                default_value='',
+                description='ROS namespace for the cam_pub node',
             ),
             DeclareLaunchArgument(
-                "threaded",
-                default_value="false",
-                description="Whether to use the threaded version of the node",
+                'threaded',
+                default_value='false',
+                description='Whether to use the threaded version of the node',
             ),
             DeclareLaunchArgument(
-                "param_file_path",
+                'param_file_path',
                 default_value=default_param_file,
-                description="Path to the parameters YAML file",
+                description='Path to the parameters YAML file',
             ),
             Node(
-                package="cam_pub",
-                executable="cam_pub_node",
-                name="cam_pub",
+                package='cam_pub',
+                executable='cam_pub_node',
+                name='cam_pub',
                 namespace=namespace,
                 parameters=[param_file_path],
-                output="screen",
+                output='screen',
                 condition=UnlessCondition(threaded),
             ),
             Node(
-                package="cam_pub",
-                executable="cam_pub_threaded_node",
-                name="cam_pub",
+                package='cam_pub',
+                executable='cam_pub_threaded_node',
+                name='cam_pub',
                 namespace=namespace,
                 parameters=[param_file_path],
-                output="screen",
+                output='screen',
                 condition=IfCondition(threaded),
             ),
         ]
